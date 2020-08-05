aa8148afeadcf0530069917c64b184bfbad6b6a7
root <root@Sezan.localdomain>
2020-08-05 08:52:26 +0600

    checking scheduling jobs

---
+++ automated.py
@@ -0,0 +1,9 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def job():
+    print('This job is run every minute.')
+
+sched.start()
